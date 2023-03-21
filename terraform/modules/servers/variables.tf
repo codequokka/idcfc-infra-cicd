b7a1c24d35d1ff8ce5bf540349c495b86b024ee2
Nobuchika Tanaka <codequokka@gmail.com>
2023-03-22 06:52:00 +0900

    Fix tflint errors

--- terraform/modules/servers/variables.tf
+++ terraform/modules/servers/variables.tf
@@ -1,5 +1,18 @@
-variable "env" {}
-variable "network_id" {}
-variable "zone" {}
-variable "template" {}
-variable "docker_hosts" {}
+variable "env" {
+  type = string
+}
+
+variable "network_id" {
+  type = string
+}
+
+variable "zone" {
+  type = string
+}
+variable "template" {
+  type = string
+}
+
+variable "docker_hosts" {
+  type = list(string)
+}
